85dccede85bcfd698d45964c2195630a5ff16758
/Z3X_ID <46043473+rianda456@users.noreply.github.com>
2019-01-08 10:11:26 +0700

    Add files via upload

---
+++ JonesLu.sh
@@ -0,0 +1,75 @@
+#Kalo Mau Recode Ijin Dulu Ama Author
+#Lu kira buat tools itu mudah
+#Lu aj tinggal copy paste done
+#Hubungi Author Tamvan :
+Bangrianda456@gmail.com
+#Atau Via Whatsapp :
+085816406954
+#!/system/xbin/bash
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+purple="\033[35;1m"
+cyan="\033[36;1m"
+white="\033[37;1m"
+red="\033[31;1m"
+clear
+echo $red"Author Tertamvan : ./Z3X_ID ft Beblly"
+echo $blue"Team : BOJONEGORO CYBER SECURITY"
+echo $cyan"Amanah : Gunakan Dengan Otak,Bukan Lutut!!"
+echo $purple"Lanjut Ea Gak Usah Banyak Bacol"
+echo $white"Tunggu 5 Detik Cuk,Sabar Aja ^_^"
+sleep 5 
+clear 
+clear 
+echo $red"Semoga aj di terima :V"
+sleep 1 
+clear 
+echo $cyan" ["red"+"cyan"]Ketika Aku Kenal Kamu"
+echo $cyan" ["red"+"cyan"] Pertama Kalinya"
+echo $cyan" ["red"+"cyan"] Saat Itu Aku Jatuh Cinta Padamu"
+echo $cyan" ["red"+"cyan"] Mau Kah Kamu Menjadi"
+echo $red"PACARKU ^_^"
+echo $blue"========> 1. Ya <========"
+echo $red"========> 2. Tidak <========"
+echo $white" 0. EXIT..."
+read -p "~[root@GuaGans^_^]> " bos;
+
+if [ $bos = 1 ] || [ $bos = 1]
+then
+clear 
+echo $blue" MAKASIH UDAH NERIMA AKU"
+sleep 1 
+clear 
+echo $blue"Makasih Udah Nerima Aku Apa Adanya"
+echo $blue"Aku Janji Kok ^_^"
+echo $blue"Akan Selalu Untukmu"
+echo $blue"Suatu Saat Aku Akan Melamarmu"
+echo $blue"I LOVE YOU"
+echo $blue"Langsung Chat Aku Aja"
+clear 
+fi 
+
+if [ $bos = 2 ] || [ $bos = 2 ]
+then 
+clear 
+echo $red"Aku Gak Terima :("
+echo $red"Tak Semudah Itu Perguso :v"
+echo $red"Gak Suka Gelud Kuy"
+echo $red"Kontak Me :v"
+clear 
+sleep 1 
+fi 
+
+if [ $bos = 0 ] || [ $bos = 0 ]
+then 
+clear 
+echo $white"Thank's Ea "
+sleep 1 
+echo $white"Udah Gunain Ni Tool"
+sleep 1 
+echo $white"Semoga Bermanfaat ^_^"
+sleep 1 
+exit 
+fi
+
